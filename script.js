16fcc6e9d7c9ac7b03be7ac0011ea73a17657c95
Franklin <frankkhuzy@gmail.com>
2023-11-01 15:16:38 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const fetchAdvice = async () => {
+const res = await fetch ("https://api.adviceslip.com/advice")
+const data = await res.json()
+
+console.log(data)
+
+
+document.getElementById('').innerHTML = ` Advice # ${data.slip.id}`
+document.getElementById('').innerHTML = `"${data.slip.advice}"`
+
+}
+fetchAdvice()
